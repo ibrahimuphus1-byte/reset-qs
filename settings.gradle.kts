ff6dc9367dbc51d24e2f2a5d8d519e965e35098c
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-18 10:58:16 +0330

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "QS Columns & Restart Tile Module"
+include(":app")
