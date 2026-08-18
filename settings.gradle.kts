ff89b6dab2e749e2f5f74d85c04c1e381f728532
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-18 11:37:23 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,7 +3,6 @@ pluginManagement {
         google()
         mavenCentral()
         gradlePluginPortal()
-        maven { url = uri("https://maven.lsposed.org/") }   // ✅ Official LSPosed repo
     }
 }
 dependencyResolutionManagement {
@@ -11,7 +10,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://maven.lsposed.org/") }   // ✅ Same repo for dependencies
     }
 }
 rootProject.name = "QS Columns & Restart Tile Module"
