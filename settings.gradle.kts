dd8a8f973705c5aba4d95ee740b16092a6842d2e
ibrahimuphus1-byte <ibrahim.uphus.1@gmail.com>
2026-08-18 11:18:26 +0330

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,11 @@ pluginManagement {
     }
 }
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://repo.xposed.info/repo") }   // ✅ Adds Xposed API
     }
 }
 rootProject.name = "QS Columns & Restart Tile Module"
